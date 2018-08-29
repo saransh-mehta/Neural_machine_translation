dfb9606b08397f5b4b45829ca25a8e8a4d66c717
saransh  mehta <saransh@saranshs-MacBook-Air.local>
2018-08-29 11:16:44 +0530

    Adding notebook for neural machine translation

---
+++ untitled.py
@@ -0,0 +1,12 @@
+'''
+Neural Machine Translation model for translating from English to French
+
+This script deals with the Data preparation part.
+
+The data is available at 
+
+https://raw.githubusercontent.com/devm2024/nmt_keras/master/fra.txt
+
+use Wget/curl -0, to get the data into the directory 'data'
+
+'''
